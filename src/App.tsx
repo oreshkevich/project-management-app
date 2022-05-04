255cac4a7939fd58cb53703ff1d1f0d90c61ccb8
Henadzi Arashkevich <oreshkevich85@mail.ru>
2022-05-04 13:24:25 +0300

    feat: add header and pages Home login NotFound Signup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Header from './components/header';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { NotFound } from './pages/NotFound';
+import { Signup } from './pages/Signup';
+import { Login } from './pages/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter basename="/">
+        <Header />
+        <main className="container content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/pages/Signup.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Signup() {
+  return (
+    <div data-testid="about-page">
+      <h1>Hello, signup </h1>
+    </div>
+  );
+}
+export { Signup };
