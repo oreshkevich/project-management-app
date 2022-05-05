b45ab554cb788c44c4ec4a4fa3d96c90e735692e
Kerll <kirillpotapovnow@gmail.com>
2022-05-05 18:45:19 +0300

    fix: fix typo

--- src/components/auth/AuthLogin.tsx
+++ src/components/auth/AuthLogin.tsx
@@ -8,17 +8,17 @@ const AuthLogin = () => {
   return (
     <Form className="auth d-flex flex-column auth mx-auto">
       <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>{t('authentication.login')}*</Form.Label>
-        <Form.Control type="email" placeholder={t('authentication.enter-login')} />
+        <Form.Label>{t('authentification.login')}*</Form.Label>
+        <Form.Control type="email" placeholder={t('authentification.enter-login')} />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>{t('authentication.password')}*</Form.Label>
-        <Form.Control type="password" placeholder={t('authentication.enter-password')} />
+        <Form.Label>{t('authentification.password')}*</Form.Label>
+        <Form.Control type="password" placeholder={t('authentification.enter-password')} />
       </Form.Group>
 
       <Button variant="outline-*" className="auth__submit mt-2 text-white" type="submit">
-        {t('authentication.submit-login')}
+        {t('authentification.submit-login')}
       </Button>
     </Form>
   );
--- src/components/auth/AuthSignup.tsx
+++ src/components/auth/AuthSignup.tsx
@@ -8,22 +8,22 @@ const AuthSignup = () => {
   return (
     <Form className="auth d-flex flex-column auth mx-auto">
       <Form.Group className="mb-3" controlId="formBasiclogin">
-        <Form.Label>{t('authentication.login')}*</Form.Label>
-        <Form.Control placeholder={t('authentication.enter-login')} />
+        <Form.Label>{t('authentification.login')}*</Form.Label>
+        <Form.Control placeholder={t('authentification.enter-login')} />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>{t('authentication.email')}*</Form.Label>
-        <Form.Control type="email" placeholder={t('authentication.enter-email')} />
+        <Form.Label>{t('authentification.email')}*</Form.Label>
+        <Form.Control type="email" placeholder={t('authentification.enter-email')} />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>{t('authentication.password')}*</Form.Label>
-        <Form.Control type="password" placeholder={t('authentication.enter-password')} />
+        <Form.Label>{t('authentification.password')}*</Form.Label>
+        <Form.Control type="password" placeholder={t('authentification.enter-password')} />
       </Form.Group>
 
       <Button variant="outline-*" className="auth__submit mt-2 text-white" type="submit">
-        {t('authentication.submit-registration')}
+        {t('authentification.submit-registration')}
       </Button>
     </Form>
   );
