6e7194a5ac873bcf3d400bac287bb33a207e499e
Kerll <kirillpotapovnow@gmail.com>
2022-05-20 16:43:02 +0300

    feat: add loading icon

--- src/components/main/Board.tsx
+++ src/components/main/Board.tsx
@@ -6,6 +6,7 @@ import { useTranslation } from 'react-i18next';
 import Card from './Card';
 import FormColumn from '../forms/formColumn/FormColumn';
 import { IColData, ITaskData } from '../../core/interfaces/interfaces';
+import LoadingIcon from '../loading/LoadingIcon';
 
 const Board = () => {
   const { t } = useTranslation();
@@ -14,12 +15,14 @@ const Board = () => {
   const [currentColumn, setCurrentColumn] = useState<IColData>();
   const [currentTasks, setCurrentTasks] = useState<ITaskData[]>();
   const [currentTask, setCurrentTask] = useState<ITaskData>();
+  const [loading, setLoading] = useState(true);
   const { id } = useParams();
 
   const handleShow = () => setShowCol(true);
 
   const getAllColumn = useCallback(async () => {
     const { data } = await getColumns(String(id));
+    setLoading(false);
     setColumns([...data].sort((a, b) => (a.order > b.order ? 1 : a.order < b.order ? -1 : 0)));
   }, [id]);
 
@@ -29,33 +32,39 @@ const Board = () => {
 
   return (
     <div>
-      <Button variant="success" onClick={handleShow}>
-        {t('header.create-col__button')}
-      </Button>
-      {showCol && (
-        <FormColumn
-          setShowCol={setShowCol}
-          getAllColumn={getAllColumn}
-          order={columns?.length || 0}
-        />
-      )}
+      {loading ? (
+        <LoadingIcon />
+      ) : (
+        <>
+          <Button variant="success" onClick={handleShow}>
+            {t('header.create-col__button')}
+          </Button>
+          {showCol && (
+            <FormColumn
+              setShowCol={setShowCol}
+              getAllColumn={getAllColumn}
+              order={columns?.length || 0}
+            />
+          )}
 
-      <div className="app-card-data">
-        {columns?.map((item: IColData) => (
-          <Card
-            data={item}
-            key={item.id}
-            getAllColumn={getAllColumn}
-            columns={columns}
-            setCurrentColumn={setCurrentColumn}
-            currentColumn={currentColumn}
-            setCurrentTasks={setCurrentTasks}
-            currentTasks={currentTasks}
-            setCurrentTask={setCurrentTask}
-            currentTask={currentTask}
-          />
-        ))}
-      </div>
+          <div className="app-card-data">
+            {columns?.map((item: IColData) => (
+              <Card
+                data={item}
+                key={item.id}
+                getAllColumn={getAllColumn}
+                columns={columns}
+                setCurrentColumn={setCurrentColumn}
+                currentColumn={currentColumn}
+                setCurrentTasks={setCurrentTasks}
+                currentTasks={currentTasks}
+                setCurrentTask={setCurrentTask}
+                currentTask={currentTask}
+              />
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 };
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,13 +2,17 @@ import { useEffect, useState } from 'react';
 import { getBoards } from '../core/api/api';
 import { BoardData } from '../core/types/types';
 import BoardCard from '../components/main/BoardCard';
+import LoadingIcon from '../components/loading/LoadingIcon';
 
 export const Main = () => {
   const [boards, setBoards] = useState<Array<BoardData>>();
+  const [loading, setLoading] = useState(true);
 
   async function getAllBoards() {
     const { data } = await getBoards();
+
     setBoards(data);
+    setLoading(false);
   }
 
   useEffect(() => {
@@ -17,9 +21,11 @@ export const Main = () => {
 
   return (
     <section className="main pt-5 pl-5">
-      {boards?.map((item: BoardData) => (
-        <BoardCard data={item} key={item.id} />
-      ))}
+      {loading ? (
+        <LoadingIcon />
+      ) : (
+        boards?.map((item: BoardData) => <BoardCard data={item} key={item.id} />)
+      )}
     </section>
   );
 };
